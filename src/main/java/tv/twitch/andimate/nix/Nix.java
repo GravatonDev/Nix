6668ed48806d221a8a19bf2de5c0590898918faa
Nisi@win10 <wolf.sf75@googlemail.com>
2022-02-04 10:38:51 +0100

    nix

---
+++ src/main/java/tv/twitch/andimate/nix/Nix.java
@@ -0,0 +1,17 @@
+package tv.twitch.andimate.nix;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Nix extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Nix.");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
